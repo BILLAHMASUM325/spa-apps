b593833ad47da4a4cec591d70ee0ffcc54e4f6a3
BILLAHMASUM325 <masumbillah3.25@outlook.com>
2022-02-10 23:13:20 +0600

    practice

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import Country from './components/Country/Country';
 import Cart from './components/Country/Cart/Cart';
+import data from './data/data.json';
 
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [cart, setCart] = useState([]);
+  const [clubs, setClubs] = useState([]);
+
+  useEffect(() => {
+      setClubs(data)
+  }, [])
 
   useEffect(() =>{
     fetch('https://restcountries.com/v3.1/all')
@@ -34,6 +40,11 @@ function App() {
       <h1>country loaded: {countries.length}</h1>
     <h4>Country added: {cart.length}</h4>
       <Cart cart ={cart}></Cart>
+      <ul>
+        {
+          clubs.map(clubs => <li>{clubs.name}</li>)
+        }
+      </ul>
       <ul>
         {
           countries.map(country => <Country details = {country} handleAddCountry = {handleAddCountry}></Country>)
